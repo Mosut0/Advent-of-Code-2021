f9b6dbdf5502c9d02df4b38d158efa95f07e7cc4
Mostafa <mos.yas123@gmail.com>
2021-12-06 03:47:17 -0500

    Added day 3 part 2

--- day3/p3.2.py
+++ day3/p3.2.py
@@ -1,26 +1,47 @@
+def most_common(rows, digit):
+    """
+    Returns most common value after arranging by column
+    Credit: Akaiyan Duquet
+    """
+    most_common_bit = 0
+    for num in rows:
+        most_common_bit += 2 * int(num[digit]) - 1
+
+    return '0' if most_common_bit < 0 else '1'
+
+
 def binary_diagnostic2(file):
     bits = open(file).read().splitlines()
 
-    gamma = ""
-    epsilon = ""
-    reports = []
-    for i in range(len(bits)):
-        for j in range(len(bits[0])):
-            if i == 0:
-                reports.append(bits[i][j])
-            else:
-                reports[j] += bits[i][j]
-
     oxygen = []
     CO2 = []
-    i = 0
-    while i != len(bits[0]) - 1:
-        if reports[0].count("0") > reports[i].count("1"):
-            gamma += "0"
-            epsilon += "1"
+
+    for i in bits:
+        if i[0] == most_common(bits, 0):
+            oxygen.append(i)
         else:
-            gamma += "1"
-            epsilon += "0"
+            CO2.append(i)
+
+    k = 1
+    while len(oxygen) > 1 or len(CO2) > 1:
+        new_oxygen = []
+        new_CO2 = []
+
+        if len(oxygen) > 1:
+            for i in oxygen:
+                if i[k] == most_common(oxygen, k):
+                    new_oxygen.append(i)
+            oxygen = new_oxygen
+
+        if len(CO2) > 1:
+            for i in CO2:
+                if i[k] != most_common(CO2, k):
+                    new_CO2.append(i)
+            CO2 = new_CO2
+
+        k += 1
+
+    return int(oxygen[0], 2) * int(CO2[0], 2)
 
 
-    return int(gamma, 2) * int(epsilon, 2)
+print(binary_diagnostic2("data3.txt"))
